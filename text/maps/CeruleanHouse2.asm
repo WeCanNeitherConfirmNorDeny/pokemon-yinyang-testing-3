bac74f561697c51ea72c9b832f7f1f07288f80d8
WeCanNeitherConfirmNorDeny <68614852+WeCanNeitherConfirmNorDeny@users.noreply.github.com>
2021-10-06 19:46:42 -0800

    Add files via upload

---
+++ text/maps/CeruleanHouse2.asm
@@ -0,0 +1,89 @@
+_CeruleanHouse2Text_74e77::
+	text "Berry Trees grow"
+	line "all over Kanto."
+
+	para "I'm sure you've"
+	line "seen them by now."
+
+	para "Did you know they"
+	line "each do something"
+    cont "different?"
+	prompt
+
+_CeruleanHouse2Text_74e7c::
+	text "Now then..."
+
+	para "Which of the 10"
+	line "Berries should I"
+	cont "describe?"
+	done
+
+_CeruleanHouse2Text_74e81::
+	text "Come visit me any"
+	line "time you wish."
+	done
+
+_OranBerryText::
+	text "Restores 10 HP"
+	line "when used."
+	prompt
+
+_SitrusBerryText::
+	text "Restores 30 HP"
+	line "when used."
+	prompt
+
+_LeppaBerryText::
+	text "Restores 10 PP"
+	line "when used."
+	prompt
+
+_PechaBerryText::
+	text "Cures a #mon of"
+	line "being Poisoned."
+	prompt
+
+_RawstBerryText::
+	text "Cures a #mon of"
+	line "being Burned."
+	prompt
+
+_AspearBerryText::
+	text "Cures a #mon of"
+	line "being Frozen."
+	prompt
+
+_ChestoBerryText::
+	text "Wakes up a #mon"
+	line "that is Asleep."
+	prompt
+
+_CheriBerryText::
+	text "Cures a #mon of"
+	line "Paralysis."
+	prompt
+
+_LumBerryText::
+	text "Cures a #mon of"
+	line "any negative"
+	cont "status condition."
+	prompt
+    
+_AcaiBerryText::
+	text "An extremely rare"
+	line "Berry."
+
+	para "It increases a"
+	line "#mon's level by"
+	cont "1 when used."
+	prompt
+
+_GoldBerryText::
+	; A mythical berry/ that is said to...
+	; Lift pokemon from/ their suffering.
+	text "A mythical berry"
+	line "that is said to..."
+
+	para "Lift #mon from"
+	line "their suffering."
+	prompt
